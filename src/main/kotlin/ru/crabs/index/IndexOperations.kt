95763e54d5ee299b3e76bb1601683e03a6dccded
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-09-09 00:04:52 +0700

    add flyway

--- src/main/kotlin/ru/crabs/index/IndexOperations.kt
+++ src/main/kotlin/ru/crabs/index/IndexOperations.kt
@@ -1,11 +1,14 @@
 package ru.crabs.index
 
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Get
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 
 @Tag(name = "Главная")
 interface IndexOperations {
 
-    @Get
+    @Get(processes = [MediaType.TEXT_PLAIN])
+    @Operation(summary = "Получить смайлик")
     fun getIndex(): String
 }
