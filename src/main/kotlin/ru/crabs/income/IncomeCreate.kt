5a4223e700e0bd1f2ff1b484254658a8aa4f2b7f
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-09-10 00:32:08 +0700

    update docs

--- src/main/kotlin/ru/crabs/income/IncomeCreate.kt
+++ src/main/kotlin/ru/crabs/income/IncomeCreate.kt
@@ -1,11 +1,13 @@
 package ru.crabs.income
 
 import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
 @Introspected
 data class IncomeCreate(
 
+        @field:NotNull
         @field:Positive(message = "amount must be positive")
         val amount: Long
 )
--- src/main/kotlin/ru/crabs/income/IncomeGet.kt
+++ src/main/kotlin/ru/crabs/income/IncomeGet.kt
@@ -1,8 +1,12 @@
 package ru.crabs.income
 
+import javax.validation.constraints.NotNull
+
 data class IncomeGet(
 
+        @field:NotNull
         var id: Long,
 
+        @field:NotNull
         val amount: Long
 )
