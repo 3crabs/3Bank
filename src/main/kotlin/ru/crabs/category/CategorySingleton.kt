c9b4e1e0908faa72ef3b0405742220ff72f52508
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-10-02 10:08:37 +0700

    add logs in category

--- src/main/kotlin/ru/crabs/category/CategorySingleton.kt
+++ src/main/kotlin/ru/crabs/category/CategorySingleton.kt
@@ -1,10 +1,13 @@
 package ru.crabs.category
 
+import org.slf4j.LoggerFactory
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class CategorySingleton : CategoryService {
+
+    private val log = LoggerFactory.getLogger(CategorySingleton::class.java)
 
     @Inject
     lateinit var categoryRepository: CategoryRepository
@@ -13,23 +16,31 @@ class CategorySingleton : CategoryService {
         if (categoryRepository.existsByName(categoryEntity.name)) {
             throw CategoryIsAlreadyInUseException()
         }
+        log.info("add category")
         return categoryRepository.save(categoryEntity)
     }
 
     override fun getCategories(): List<CategoryEntity> {
+        log.info("get categories")
         return categoryRepository.findAllByCategoryIdIsNull()
     }
 
     override fun deleteCategory(id: Long): CategoryEntity? {
         val c = categoryRepository.findOneById(id)
-        c?.id?.let { categoryRepository.deleteById(it) }
+        c?.id?.let {
+            log.info("delete category")
+            categoryRepository.deleteById(it)
+        }
         return c
     }
 
     override fun updateCategory(id: Long, categoryEntity: CategoryEntity): CategoryEntity? {
         val c = categoryRepository.findOneById(id)
         c?.name = categoryEntity.name
-        c?.id?.let { categoryRepository.deleteById(it) }
+        c?.id?.let {
+            log.info("update category")
+            categoryRepository.deleteById(it)
+        }
         return c
     }
 }
